3a2d13f2dacc4c086ffc3610e6beacd483e92efa
Igor <egor@sgf.lt>
2023-09-21 11:20:38 +0300

    Fill in the industries

--- task-1/main.R
+++ task-1/main.R
@@ -11,3 +11,9 @@ summary(data)
 describeBy(data, "Industry")
 
 # 4. praleistų reikšmių užpildymas
+# fill in industries
+data[data$Industry == "", ]
+data[14, "Industry"] <- "IT Services"
+data[15, "Industry"] <- "Financial Services"
+data[136, "Industry"] <- "Construction"
+unique(data$Industry)
