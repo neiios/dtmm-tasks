7433a5cc77a2670aca742f81fd642b19046ffa74
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-23 15:42:51 +0300

    Handle outliers and correlations

--- task-1/main.R
+++ task-1/main.R
@@ -1,5 +1,14 @@
-library(psych)
-library(tidyverse)
+install.packages("psych")
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("Hmisc")
+install.packages("stringi")
+
+
+# library("psych")
+library("tidyverse")
+library("ggplot2")
+library("Hmisc")
 
 # 1. Failo nuskaitymas
 getwd()
@@ -33,9 +42,9 @@ convert_to_numeric <- function(df) {
   
 }
 
-# 4. Praleistų reikšmių užpildymas
+# 3. Praleistų reikšmių užpildymas
 
-# 4.1. Faktinis užpildymas
+# 3.1. Faktinis užpildymas
 fill_missing_states <- function(df) {
   states_and_cities <- list(
     "New York" = "NY",
@@ -52,7 +61,7 @@ fill_missing_states <- function(df) {
   return(df)
 }
 
-# 4.2. Užpildymas naudojant mediana
+# 3.2. Užpildymas naudojant mediana
 median_imputation_filling <- function(df) {
 
   industries <- unique(df$Industry)
@@ -86,7 +95,64 @@ adjust_expenses <- function(df) {
   return(df)
 }
 
+plot_outliers_by_industry <- function(df, metric) {
+  ggplot(df, aes_string(x = "Industry", y = metric)) + 
+    geom_boxplot(aes(color=Industry)) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    guides(color=FALSE)
+}
 
+
+
+identify_outliers_by_industry <- function(df, metric) {
+  
+  outliers <- list()
+  
+  industries <- unique(df$Industry)
+  
+  for (industry in industries) {
+    industry_df <- df[df$Industry == industry, ]
+    
+    Q1 <- quantile(industry_df[[metric]], 0.25, na.rm = TRUE)
+    Q3 <- quantile(industry_df[[metric]], 0.75, na.rm = TRUE)
+    H <- Q3 - Q1
+    
+    inner_barrier <- c(Q1 - 1.5*H, Q3 + 1.5*H)
+    outer_barrier <- c(Q1 - 3*H, Q3 + 3*H)
+    
+    extreme_outliers <- industry_df[industry_df[[metric]] < outer_barrier[1] | industry_df[[metric]] > outer_barrier[2], ]
+    mild_outliers <- industry_df[industry_df[[metric]] < inner_barrier[1] | industry_df[[metric]] > inner_barrier[2], ]
+    
+    outliers[[industry]] <- list(extreme_outliers = extreme_outliers, mild_outliers = mild_outliers)
+  }
+  
+  return(outliers)
+}
+
+remove_extreme_outliers_by_industry <- function(df, metric) {
+  outliers <- identify_outliers_by_industry(df, metric)
+  industries <- unique(df$Industry)
+  
+  for (industry in industries) {
+    extreme_outliers <- outliers[[industry]]$extreme_outliers
+    df <- df[!(row.names(df) %in% row.names(extreme_outliers)), ]
+  
+  }
+  return(df)
+}
+
+find_correlations <- function(df) {
+  
+  # Select only numeric columns
+  df_numeric <- select(df,Employees, Revenue, Expenses, Profit, Growth)
+  
+  # Koreliacijos koeficientai
+  rcorr(as.matrix(df_numeric))
+  
+  # Koreliacijos matrica
+  source ("http://www.sthda.com/upload/rquery_cormat.r")
+  rquery.cormat(df_numeric)
+}
 
 fin <- data
 fin <- convert_to_numeric(fin)
@@ -105,8 +171,26 @@ fin <- median_imputation_filling(fin)
 fin <- adjust_profit(fin)
 fin <- adjust_expenses(fin)
 
+fin <- fin[!is.na(fin$Industry), ]
+
 empty_values_test <- mapply(anyNA, fin)
 empty_values_test
 
+plot_outliers_by_industry(fin, "Revenue")
+identify_outliers_by_industry(fin,"Revenue")
+fin <- remove_extreme_outliers_by_industry(fin, "Revenue")
 
+plot_outliers_by_industry(fin, "Expenses")
+identify_outliers_by_industry(fin,"Expenses")
+fin <- remove_extreme_outliers_by_industry(fin, "Expenses")
+
+plot_outliers_by_industry(fin, "Growth")
+identify_outliers_by_industry(fin,"Growth")
+fin <- remove_extreme_outliers_by_industry(fin, "Growth")
+
+plot_outliers_by_industry(fin, "Inception")
+identify_outliers_by_industry(fin,"Inception")
+fin <- remove_extreme_outliers_by_industry(fin, "Inception")
+
+find_correlations(fin)
 
